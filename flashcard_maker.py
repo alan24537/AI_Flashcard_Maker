c4b4b9c0483d0f633cd33b7641eb3dfb497c1492
WendyZ391 <77943033+WendyZ391@users.noreply.github.com>
2023-05-06 17:30:16 -0400

    conversion function
    :)))))))))))
    cup stacking 101

--- flashcard_maker.py
+++ flashcard_maker.py
@@ -1,6 +1,16 @@
 import openai
 openai.api_key = "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"
 
+def conversion(st,num):
+    st = st.split("\n")
+    news = list(filter(None, st))
+
+    newnews = []
+    for i in range(num):
+        card = [news[i*2],news[i*2+1]]
+        newnews.append(card)
+    return newnews
+
 def generate_flashcards(number_of_cards, subject, grade):
     prompt = f"""
     Can you make {number_of_cards} flashcard questions and answers for {grade} {subject}? 
@@ -16,7 +26,7 @@ def generate_flashcards(number_of_cards, subject, grade):
         stop=None,
         temperature=0.7,
     )
-
-    print(response.choices[0].text)
+    
+    print(conversion(response.choices[0].text,number_of_cards))
     
 generate_flashcards(10, "imaginary", "grade 11")
